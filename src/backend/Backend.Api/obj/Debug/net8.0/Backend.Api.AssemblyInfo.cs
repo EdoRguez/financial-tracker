461320006e9319e1ef5023154f294f197b092308
edorguez <edro998@gmail.com>
2025-02-18 13:04:32 -0400

    Remove temp files

--- src/backend/Backend.Api/obj/Debug/net8.0/Backend.Api.AssemblyInfo.cs
+++ src/backend/Backend.Api/obj/Debug/net8.0/Backend.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674a8d26d261312a39136a3ff569e7d7810da5c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d8eaa1a378c7982cc11cd58465b1fd3fe8e325")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
